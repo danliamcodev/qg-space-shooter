3e23d3846053119d6e82479038e52beaac3a05c3
Dan Liamco <dan.liamco@yahoo.com>
2023-11-09 05:40:03 +0800

    Update:  - Tweaked wave behavior

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -15,15 +15,18 @@ public class WaveSpawner : MonoBehaviour
 
 	public IEnumerator SpawnWavesTask()
 	{
+		print("SPAWNING");
 		int waves_spawned = 0;
 		while (waves_spawned < m_wave_order.wave_order.Count)
 		{
-			GameObject wave = Instantiate(m_wave_order.wave_order[waves_spawned], transform.parent);
+			GameObject wave = Instantiate(m_wave_order.wave_order[waves_spawned], transform);
 			wave.transform.position = transform.position;
 			wave.SetActive(true);
 			waves_spawned++;
 
 			yield return new WaitForSeconds(m_spawn_interval);
 		}
+
+		print("WAVES DONE");
 	}
 }
