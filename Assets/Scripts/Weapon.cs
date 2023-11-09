e18e7c636525c3b794c41aa3b1d6d6b0e85a6211
Dan Liamco <dan.liamco@yahoo.com>
2023-11-09 21:38:36 +0800

    Addition:  - Added SFX to player shooting

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,8 +6,10 @@ public class Weapon : MonoBehaviour
 {
     [Header("Parameter")]
     [SerializeField] float m_fire_rate = 0.1f;
+    [SerializeField] AudioClip m_shoot_sfx;
 
     [Header("References")]
+    [SerializeField] SoundManager m_sound_manager;
     [SerializeField] List<BulletSpawner> m_bullet_spawners;
 
     float m_fire_cooldown = 0f;
@@ -27,6 +29,12 @@ public class Weapon : MonoBehaviour
         {
             bullet_spawner.SpawnBullet();
         }
+
+        if (m_shoot_sfx != null)
+        {
+            m_sound_manager.PlaySFX(m_shoot_sfx);
+        }
+
 
         m_fire_cooldown = m_fire_rate;
     }
